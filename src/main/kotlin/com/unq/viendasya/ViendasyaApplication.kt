3c8ed2ec8a0298ab9a21584211fab59fc964746f
PabloSaba <psabaliauskas@teco.com.ar>
2019-09-17 18:21:09 -0300

    added Swagger

--- src/main/kotlin/com/unq/viendasya/ViendasyaApplication.kt
+++ src/main/kotlin/com/unq/viendasya/ViendasyaApplication.kt
@@ -1,13 +1,28 @@
 package com.unq.viendasya
 
+import SwaggerConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableJpaAuditing
-class ViendasyaApplication
+@Import(SwaggerConfiguration::class)
+class ViendasyaApplication: WebMvcConfigurer {
+
+
+
+	override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/")
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<ViendasyaApplication>(*args)
 }
+
