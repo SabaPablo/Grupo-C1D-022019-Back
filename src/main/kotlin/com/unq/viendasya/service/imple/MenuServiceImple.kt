3f25a28e4ae665fd97466c199f90ec931b50a9de
sirDemian <sirdemian@gmail.com>
2019-10-27 18:55:30 -0300

    create menu

--- src/main/kotlin/com/unq/viendasya/service/imple/MenuServiceImple.kt
+++ src/main/kotlin/com/unq/viendasya/service/imple/MenuServiceImple.kt
@@ -4,6 +4,7 @@ import com.unq.viendasya.controller.apiModels.MiniMenu
 import com.unq.viendasya.exception.CurrencyMenuException
 import com.unq.viendasya.model.Menu
 import com.unq.viendasya.repository.MenuRepository
+import com.unq.viendasya.repository.UserRepository
 import com.unq.viendasya.service.MenuService
 import org.joda.time.LocalDate
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,19 +15,23 @@ import java.util.*
 import javax.transaction.Transactional
 
 @Service
-class MenuServiceImple(@Autowired private val  dao: MenuRepository) : MenuService {
+class MenuServiceImple(@Autowired private val  dao: MenuRepository, @Autowired private val service: UserRepository) : MenuService {
 
     override fun createMenu(data: MiniMenu): Menu? {
+        val provider = service.findById(data.idProvider)
         val menu = Menu.Builder().name(data.name)
                 .cantMin(data.cantMin)
                 .cantMax(data.cantMax)
                 .cantMaxPeerDay(data.cantMaxPerDay)
-                .deliveryTime(data.DeliveryTime)
+                .description(data.description)
+                .urlImage(data.urlImage)
+                //.deliveryTime(data.DeliveryTime)
                 .deliveryValue(data.priceDelivery.toDouble())
-                .expiration(LocalDate(data.dateEnd))
+                //.expiration(LocalDate(data.dateEnd))
                 .price(data.price.toDouble())
                 .priceCantMin(data.priceMin.toDouble())
-                .priceCantMax(data.priceMax.toDouble()).build()
+                .priceCantMax(data.priceMax.toDouble())
+                .provider(provider.get()).build()
         return dao.save(menu)
     }
 
