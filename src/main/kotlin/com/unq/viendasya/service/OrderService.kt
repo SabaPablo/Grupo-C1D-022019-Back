c1f07d199f0e41c1bacc8e74dc0fdfa0032c7470
PabloSaba <psabaliauskas@teco.com.ar>
2019-12-01 02:20:52 -0300

    rate and close orders

--- src/main/kotlin/com/unq/viendasya/service/OrderService.kt
+++ src/main/kotlin/com/unq/viendasya/service/OrderService.kt
@@ -3,8 +3,11 @@ package com.unq.viendasya.service
 import com.unq.viendasya.controller.apiModels.MaxiOrder
 import com.unq.viendasya.controller.apiModels.MiniOrder
 import com.unq.viendasya.model.Order
+import java.time.LocalDateTime
 
 interface OrderService {
     fun createOrder(data: MiniOrder): Order?
     fun findByUserId(userId: Int): List<MaxiOrder>
+    fun closeOrder(dateTime: LocalDateTime)
+    fun valuateOrder(orderId: Int, valoration: Int): MaxiOrder?
 }
