e4d127725708fdeb9a6b981acb285c39d8ecd32b
PabloSaba <psabaliauskas@teco.com.ar>
2019-12-01 15:21:29 -0300

    valid json create user and create menu

--- src/main/kotlin/com/unq/viendasya/controller/apiModels/MiniMenu.kt
+++ src/main/kotlin/com/unq/viendasya/controller/apiModels/MiniMenu.kt
@@ -1,9 +1,20 @@
 package com.unq.viendasya.controller.apiModels
 
-data class MiniMenu(val name: String,
-                    val description: String,
-                    val category: MutableList<String>,
-                    val priceDelivery: Int,
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class MiniMenu(
+        @field:Size(min = 4, max = 30)
+        val name: String,
+        @field:Size(min = 20, max = 40)
+        val description: String,
+        @field:NotNull
+        val category: MutableList<String>,
+        @field:Min(10)
+        @field:Max(40)
+        val priceDelivery: Int,
                     val dateInit: String,
                     val dateEnd: String,
                     //val turn: String,
@@ -11,8 +22,17 @@ data class MiniMenu(val name: String,
                     val urlImage: String,
                     val idProvider: Int,
                     val price: String,
-                    val cantMin: Int,
-                    val priceMin: String,
-                    val cantMax: Int,
-                    val priceMax: String,
-                    val cantMaxPerDay: Int)
+        @field:Min(10)
+        @field:Max(70)
+        val cantMin: Int,
+        @field:Min(0)
+        @field:Max(1000)
+        val priceMin: String,
+        @field:Min(40)
+        @field:Max(150)
+        val cantMax: Int,
+        @field:Min(0)
+        @field:Max(1000)
+        val priceMax: String,
+        @field:NotNull
+        val cantMaxPerDay: Int)
